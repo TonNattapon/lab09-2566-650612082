885659770f1cd74dae91ce3b974843ed501e1ff2
= <=>
2023-07-21 22:44:30 +0700

    Completed

--- src/app/page.js
+++ src/app/page.js
@@ -10,16 +10,25 @@ import { useState } from "react";
 export default function Home() {
   //tasks = array of {id: string, title: string, completed: boolean}
   const [tasks, setTasks] = useState([]);
+  const [count1, setCount1] = useState(0);
+  const [count2, setCount2] = useState(0);
 
   const addTask = (newTaskTitle) => {
     const newTask = { id: nanoid(), title: newTaskTitle, completed: false };
     const newTasks = [...tasks, newTask];
+    setCount1(count1 + 1);
     setTasks(newTasks);
   };
 
   const deleteTask = (taskId) => {
     const newTasks = tasks.filter((task) => task.id !== taskId);
+    setCount1(count1 - 1);
     setTasks(newTasks);
+
+    const taskToDelete = tasks.find((task) => task.id === taskId);
+    if (taskToDelete && taskToDelete.completed) {
+      setCount2(Math.max(count2 - 1, 0));
+    }
   };
 
   const toggleDoneTask = (taskId) => {
@@ -28,8 +37,15 @@ export default function Home() {
     const newTasks = structuredClone(tasks);
     //search for a task based on condition
     const task = newTasks.find((x) => x.id === taskId);
+    const isTaskCompleted = task.completed;
     task.completed = !task.completed;
     setTasks(newTasks);
+
+    if (task.completed && !isTaskCompleted) {
+      setCount2(count2 + 1);
+    } else if (!task.completed && isTaskCompleted) {
+      setCount2(Math.max(count2 - 1, 0));
+    }
   };
 
   return (
@@ -41,7 +57,7 @@ export default function Home() {
       <div style={{ maxWidth: "400px" }} className="mx-auto">
         {/* Task summary */}
         <p className="text-center text-secondary fst-italic">
-          All (...) Done (...)
+          All ({count1}) Done ({count2})
         </p>
         {/* task input */}
         <TaskInput addTaskFunc={addTask} />
@@ -60,7 +76,11 @@ export default function Home() {
       </div>
 
       {/* //footer section */}
-      <Footer year="2023" fullName="Chayanin Suatap" studentId="12345678" />
+      <Footer
+        year="2023"
+        fullName="Natthapon Chanaveroj"
+        studentId=" 650612082"
+      />
     </div>
   );
 }
